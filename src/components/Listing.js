47429163cff5ae487e1db29873d852921607cc75
unknown <codename100.alan@gmail.com>
2018-09-03 19:42:08 -0400

    Displayed Rider List

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
 class Listing extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            riders: []
+        }
+    }
+
+    componentWillMount(){
+        fetch('https://blooming-mesa-21883.herokuapp.com/riders')
+        .then(res=>res.json())
+        .then(data=> this.setState({riders:data.riders}));
+    }
+    componentWillUnmount() {
+        this.isCancelled = true;
+    }
   render() {
+      const riderListView = this.state.riders.map(rider=>(
+        <div key={rider.id}>
+            <h3>{rider.username}</h3>
+            <p>{rider.address}</p>
+        </div>
+      ));
     return (
       <div>
-        list
+        <h1>Rider List </h1>
+            {riderListView}
       </div>
     )
   }
