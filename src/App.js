02347793202ef481f525464d3c3c7baae083607b
unknown <codename100.alan@gmail.com>
2018-09-03 12:39:58 -0400

    Added Nav links for 2 components in 2 seperate page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
-
+import Register from './components/Register';
+import Listing from './components/Listing';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path ='/' component={Home} />
+      <div>
+        <Navigation />
+          <Switch>
+            <Route path ='/' component={Home} exact />
+            <Route path ='/register' component={Register} />
+            <Route path ='/listing' component={Listing} />
+            <Route component={Error} />
+          </Switch>
+      </div>
       </BrowserRouter>
     );
   }
---
+++ src/components/Listing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Listing extends Component {
+  render() {
+    return (
+      <div>
+        list
+      </div>
+    )
+  }
+}
+
+export default Listing;
