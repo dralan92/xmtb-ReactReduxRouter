eefdd477fe58e96fb6bb9d143f84a07cb329f755
unknown <codename100.alan@gmail.com>
2018-09-04 13:25:06 -0400

    populated the list with bootstrap

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -19,28 +19,47 @@ class Listing extends Component {
     }
   render() {
       const riderListView = this.state.riders.map(rider=>(
-        <div key={rider.id}>
-            <h3>{rider.username}</h3>
-            <p>{rider.address}</p>
-        </div>
+        <li key = 'rider.id' class="list-group-item ">
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-sm-2'>
+                        <strong>{rider.username}</strong>
+                    </div>
+                    <div className='col-sm-2'>
+                        <span>Email: {rider.email}</span>
+                    </div>
+                    <div className='col-sm-2'>
+                        <span>Phone: {rider.phone}</span>
+                    </div>
+                    <div className='col-sm-2'>
+                        <span>Address: {rider.address}</span>
+                    </div>  
+                    <div className='col-sm-2'>
+                        <span>Zip: {rider.zip}</span>
+                    </div>
+                    <div className='col-sm-2'>
+                        <span>Date of Birth: {rider.dateOfBirth}</span>
+                    </div>    
+                </div>
+            </div>
+        
+        
+        </li>
       ));
     return (
-      <div>
-        
-            
-        <div className='wrapper'>
-            <div className='main'>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-xs-2'>
-                            <h1>Rider List </h1>
-                                {riderListView}
-                        </div>
+        <div>
+            <h1 className='reg_header'>List Of Registered Riders</h1>
+            <div className='container'>
+                <div className='row' >
+                    <div className = 'col'>
+                        <ul className="list-group">
+                            {riderListView}
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
-      </div>
+      
     )
   }
 }
